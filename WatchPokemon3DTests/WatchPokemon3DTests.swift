7d70e6da26890c0892462d61644f72cab84bb19e
olexey27 <allxyog@gmail.com>
2024-09-29 20:46:51 +0200

    Initial Commit

---
+++ WatchPokemon3DTests/WatchPokemon3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchPokemon3DTests.swift
+//  WatchPokemon3DTests
+//
+//  Created by Alexej K on 9/29/24.
+//
+
+import Testing
+@testable import WatchPokemon3D
+
+struct WatchPokemon3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
